65059286f0e055e020f677c01b889f582b3d12b0
Polushkin <ccfytrr@mail.ru>
2015-05-06 18:26:46 +0400

    datasource

--- src/java/classes/TestConnect.java
+++ src/java/classes/TestConnect.java
@@ -1,10 +1,10 @@
 package classes;
 
+import javax.naming.NamingException;
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import javax.naming.NamingException;
 
 /**
  * Created by Саша on 18.03.2015.
@@ -12,11 +12,12 @@ import javax.naming.NamingException;
 public class TestConnect {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException, IOException, JAXBException, NamingException {
-        DataSourcePool dataSource = new DataSourcePool();
+        //DataSourcePool dataSource = new DataSourcePool();
+        DataSource dataSource = new DataSource("root", "11");
         DataBaseStudentDaoImpl dataBaseStudentDao = new DataBaseStudentDaoImpl(dataSource.getConnection());
         DataBaseGroupDaoImpl dataBaseGroupDao = new DataBaseGroupDaoImpl(dataSource.getConnection());
 
-        dataBaseGroupDao.export("groups.xml");        
+        dataBaseGroupDao.export("groups.xml");
 
         ArrayList list = dataBaseGroupDao.getAllGroups();
         for (int i = 0; i < list.size(); i++) {
